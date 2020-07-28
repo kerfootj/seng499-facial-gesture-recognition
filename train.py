dfb30ea2facf09f9d55cbb74740e6865de2f6638
ChrisWNorton <chriswsnorton@gmail.com>
2020-07-28 10:18:50 -0700

    plot model

--- train.py
+++ train.py
@@ -8,25 +8,38 @@ from keras import regularizers
 from keras.regularizers import l1
 from keras.layers.core import Activation, Flatten, Dropout, Dense
 from keras.optimizers import RMSprop, SGD, Adam
+import matplotlib.pyplot as plt
 import numpy
 import os
 
 number_classes = 7
 image_row, image_col = 48, 48
 batch_size = 512
-number_train_samples = 199
 
 x_train = []
 y_train = []
+x_test = []
+y_test = []
 
-with open ('test_data/test_data.csv', mode = 'r', encoding='utf-8-sig') as csv_file:
+with open ('training_data/icml_face_data.csv', mode = 'r', encoding='utf-8-sig') as csv_file:
     csv_reader = csv.DictReader(csv_file)
+    i = 0
     for row in csv_reader:   
         dimensions_array = numpy.fromstring(row['pixels'], sep = " ").reshape((48, 48, 1))
         x_train.append(dimensions_array)
         y = numpy.zeros(7)
         y[int(row['emotion'])] = 1 
         y_train.append(y)
+
+with open ('test_data/test.csv', mode = 'r', encoding='utf-8-sig') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    i = 0
+    for row in csv_reader:   
+        dimensions_array = numpy.fromstring(row['pixels'], sep = " ").reshape((48, 48, 1))
+        x_test.append(dimensions_array)
+        y = numpy.zeros(7)
+        y[int(row['emotion'])] = 1 
+        y_test.append(y)
 
 # print(numpy.asarray(x_train))
 
@@ -46,8 +59,11 @@ model.add(Flatten())
 model.add(Activation("softmax"))
 # Summarize
 model.summary()
+
 x_train = numpy.asarray(x_train)
 y_train = numpy.asarray(y_train)
+x_test = numpy.asarray(x_train)
+y_test = numpy.asarray(y_train)
 
 # Compile model
 model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
@@ -60,17 +76,24 @@ checkpoint = keras.callbacks.ModelCheckpoint(filepath,
                                              verbose=1,
                                              save_best_only=True,
                                              mode='max',
-                                             save_freq='epoch'
                                              )
 callbacks = [checkpoint]
 epochs = 150
 model_info = model.fit(
     x = x_train,
     y = y_train,
-    validation_data=(x_train, y_train),
-    steps_per_epoch=number_train_samples,
-    epochs=2,
-    callbacks=callbacks
+    # batch_size=512,
+    validation_data=(x_test, y_test),
+    # steps_per_epoch=10,
+    epochs=100,
+    callbacks=callbacks,
+    validation_steps = 20
 )
 
-print(model_info)
+plt.plot(model_info.history['loss'])
+plt.plot(model_info.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
